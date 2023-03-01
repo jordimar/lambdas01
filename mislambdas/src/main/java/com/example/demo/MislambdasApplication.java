c4f9e7e01c0a5f889b1e511f1e10fef9770243db
Jordi Marques <jordi.marqu@gmail.com>
2023-03-01 11:46:11 +0100

    Inicio

---
+++ mislambdas/src/main/java/com/example/demo/MislambdasApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MislambdasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MislambdasApplication.class, args);
+		
+		
+		System.out.println("Hola mundo");  
+	}
+
+}
